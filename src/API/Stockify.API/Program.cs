b045bb15a478222173aaa0dac87a53a46704479d
Alexey Kostyuchenko <kosttchka@gmail.com>
2024-06-19 20:49:19 +0200

    Add Serilog logging

--- src/API/Stockify.API/Program.cs
+++ src/API/Stockify.API/Program.cs
@@ -1,6 +1,13 @@
+using Serilog;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((context, config) => config.ReadFrom.Configuration(context.Configuration));
+    
 WebApplication app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.UseSerilogRequestLogging();
 
 app.Run();
