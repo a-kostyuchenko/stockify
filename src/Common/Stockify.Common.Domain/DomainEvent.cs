19a320cb3583361b5725e1a5102af19ecc4af2aa
Alexey Kostyuchenko <kosttchka@gmail.com>
2024-06-20 20:16:49 +0200

    Add error representation

--- src/Common/Stockify.Common.Domain/DomainEvent.cs
+++ src/Common/Stockify.Common.Domain/DomainEvent.cs
@@ -1,19 +1,12 @@
 namespace Stockify.Common.Domain;
 
-public abstract class DomainEvent : IDomainEvent
+public abstract class DomainEvent(Guid id, DateTime occurredOnUtc) 
+    : IDomainEvent
 {
-    protected DomainEvent()
+    protected DomainEvent() : this(Guid.NewGuid(), DateTime.UtcNow)
     {
-        Id = Guid.NewGuid();
-        OccurredOnUtc = DateTime.UtcNow;
     }
 
-    protected DomainEvent(Guid id, DateTime occurredOnUtc)
-    {
-        Id = id;
-        OccurredOnUtc = occurredOnUtc;
-    }
-    
-    public Guid Id { get; init; }
-    public DateTime OccurredOnUtc { get; init; }
+    public Guid Id { get; init; } = id;
+    public DateTime OccurredOnUtc { get; init; } = occurredOnUtc;
 }
