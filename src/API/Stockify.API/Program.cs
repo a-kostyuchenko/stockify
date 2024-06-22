83b2973f557f5498d042f39dca71c0440f44be50
Alexey Kostyuchenko <kosttchka@gmail.com>
2024-06-22 14:28:21 +0200

    Add global exception handler

--- src/API/Stockify.API/Program.cs
+++ src/API/Stockify.API/Program.cs
@@ -1,12 +1,16 @@
 using Serilog;
+using Stockify.API.Infrastructure;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.Host.UseSerilog((context, config) => config.ReadFrom.Configuration(context.Configuration));
+
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddProblemDetails();
+
+builder.Services.AddEndpointsApiExplorer();
     
 WebApplication app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
 
 app.UseSerilogRequestLogging();
 
