7b94df5bbd2e0a5a4c66a5e59e0d00d5b935522b
Alexey Kostyuchenko <kosttchka@gmail.com>
2024-06-22 14:24:04 +0200

    Add infrastructure DI

---
+++ src/Common/Stockify.Common.Infrastructure/InfrastructureConfiguration.cs
@@ -0,0 +1,26 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Npgsql;
+using Stockify.Common.Application.Clock;
+using Stockify.Common.Application.Data;
+using Stockify.Common.Infrastructure.Clock;
+using Stockify.Common.Infrastructure.Data;
+
+namespace Stockify.Common.Infrastructure;
+
+public static class InfrastructureConfiguration
+{
+    public static IServiceCollection AddInfrastructure(
+        IServiceCollection services,
+        string databaseConnection)
+    {
+        services.TryAddSingleton<IDateTimeProvider, DateTimeProvider>();
+        
+        NpgsqlDataSource npgsqlDataSource = new NpgsqlDataSourceBuilder(databaseConnection).Build();
+        services.TryAddSingleton(npgsqlDataSource);
+        
+        services.TryAddScoped<IDbConnectionFactory, DbConnectionFactory>();
+
+        return services;
+    }
+}
