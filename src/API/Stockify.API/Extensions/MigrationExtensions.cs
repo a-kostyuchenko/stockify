b2010490ff7d23d6085ab8f2bff8bbf3546f363c
Alexey Kostyuchenko <kosttchka@gmail.com>
2024-06-23 14:47:33 +0200

    Add users migration

---
+++ src/API/Stockify.API/Extensions/MigrationExtensions.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using Stockify.Modules.Users.Infrastructure.Database;
+
+namespace Stockify.API.Extensions;
+
+public static class MigrationExtensions
+{
+    public static void ApplyMigrations(this IApplicationBuilder app)
+    {
+        using IServiceScope scope = app.ApplicationServices.CreateScope();
+
+        ApplyMigration<UsersDbContext>(scope);
+    }
+    
+    private static void ApplyMigration<TDbContext>(IServiceScope scope)
+        where TDbContext : DbContext
+    {
+        using TDbContext context = scope.ServiceProvider.GetRequiredService<TDbContext>();
+
+        context.Database.Migrate();
+    }
+}
