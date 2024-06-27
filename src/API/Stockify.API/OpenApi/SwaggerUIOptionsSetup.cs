c95239a55777948baf8b99a42b38455d688bfabb
Alexey Kostyuchenko <kosttchka@gmail.com>
2024-06-27 17:35:17 +0200

    Add API versioning

--- src/API/Stockify.API/OpenApi/SwaggerUIOptionsSetup.cs
+++ src/API/Stockify.API/OpenApi/SwaggerUIOptionsSetup.cs
@@ -1,10 +1,22 @@
+using Asp.Versioning.ApiExplorer;
 using Microsoft.Extensions.Options;
 using Swashbuckle.AspNetCore.SwaggerUI;
 
 namespace Stockify.API.OpenApi;
 
-internal sealed class SwaggerUIOptionsSetup : IConfigureOptions<SwaggerUIOptions>
+internal sealed class SwaggerUIOptionsSetup(IApiVersionDescriptionProvider descriptionProvider) 
+    : IConfigureOptions<SwaggerUIOptions>
 {
-    public void Configure(SwaggerUIOptions options) => 
+    public void Configure(SwaggerUIOptions options)
+    {
         options.DisplayRequestDuration();
+
+        foreach (ApiVersionDescription description in descriptionProvider.ApiVersionDescriptions)
+        {
+            string url = $"/swagger/{description.GroupName}/swagger.json";
+            string name = description.GroupName.ToUpperInvariant();
+
+            options.SwaggerEndpoint(url, name);
+        }
+    }
 }
--- src/API/Stockify.API/Program.cs
+++ src/API/Stockify.API/Program.cs
@@ -1,3 +1,5 @@
+using Asp.Versioning;
+using Asp.Versioning.Builder;
 using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Serilog;
@@ -7,6 +9,7 @@ using Stockify.API.OpenApi;
 using Stockify.Common.Application;
 using Stockify.Common.Infrastructure;
 using Stockify.Common.Infrastructure.Configuration;
+using Stockify.Common.Presentation.Endpoints;
 using Stockify.Modules.Users.Infrastructure;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
@@ -37,6 +40,15 @@ builder.Services.AddUsersModule(builder.Configuration);
     
 WebApplication app = builder.Build();
 
+ApiVersionSet apiVersionSet = app.NewApiVersionSet()
+    .HasApiVersion(new ApiVersion(1))
+    .ReportApiVersions()
+    .Build();
+
+RouteGroupBuilder versionedGroup = app
+    .MapGroup("api/v{version:apiVersion}")
+    .WithApiVersionSet(apiVersionSet);
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -57,5 +69,7 @@ app.UseExceptionHandler();
 app.UseAuthentication();
 
 app.UseAuthorization();
+
+app.MapEndpoints(versionedGroup);
 
 app.Run();
