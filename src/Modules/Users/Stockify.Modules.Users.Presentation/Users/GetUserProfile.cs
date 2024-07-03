af0e9c55172ea9faea4bf0e3b9e77b2a99cfe874
Alexey Kostyuchenko <kosttchka@gmail.com>
2024-07-03 19:35:12 +0200

    Get user profile endpoint

---
+++ src/Modules/Users/Stockify.Modules.Users.Presentation/Users/GetUserProfile.cs
@@ -0,0 +1,29 @@
+using System.Security.Claims;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Stockify.Common.Domain;
+using Stockify.Common.Infrastructure.Authentication;
+using Stockify.Common.Presentation.Endpoints;
+using Stockify.Common.Presentation.Results;
+using Stockify.Modules.Users.Application.Users.Queries.GetById;
+using Stockify.Modules.Users.Domain.Users;
+
+namespace Stockify.Modules.Users.Presentation.Users;
+
+internal sealed class GetUserProfile : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("users/profile", async (ISender sender, ClaimsPrincipal claimsPrincipal) =>
+        {
+            var query = new GetUserByIdQuery(UserId.FromValue(claimsPrincipal.GetUserId()));
+
+            Result<UserResponse> result = await sender.Send(query);
+            
+            return result.Match(Results.Ok, ApiResults.Problem);
+        })
+        .RequireAuthorization();
+    }
+}
