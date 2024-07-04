1a43266db8dd00852c7a3b07ecaafe5359c2b251
Alexey Kostyuchenko <kosttchka@gmail.com>
2024-07-04 19:09:10 +0200

    Add domain event handler abstraction

---
+++ src/Common/Stockify.Common.Application/Messaging/IDomainEventHandler.cs
@@ -0,0 +1,14 @@
+using Stockify.Common.Domain;
+
+namespace Stockify.Common.Application.Messaging;
+
+public interface IDomainEventHandler<in TDomainEvent> : IDomainEventHandler
+    where TDomainEvent : IDomainEvent
+{
+    Task Handle(TDomainEvent domainEvent, CancellationToken cancellationToken = default);
+}
+
+public interface IDomainEventHandler
+{
+    Task Handle(IDomainEvent domainEvent, CancellationToken cancellationToken = default);
+}
