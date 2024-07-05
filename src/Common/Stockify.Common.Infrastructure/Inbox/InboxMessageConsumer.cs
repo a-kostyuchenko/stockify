b3788f86c94901ddd5954335de9b45ec41c5a70a
Alexey Kostyuchenko <kosttchka@gmail.com>
2024-07-05 13:02:54 +0200

    Add inbox message consumer

---
+++ src/Common/Stockify.Common.Infrastructure/Inbox/InboxMessageConsumer.cs
@@ -0,0 +1,8 @@
+namespace Stockify.Common.Infrastructure.Inbox;
+
+public sealed class InboxMessageConsumer(Guid inboxMessageId, string name)
+{
+    public Guid InboxMessageId { get; init; } = inboxMessageId;
+    
+    public string Name { get; init; } = name;
+}
