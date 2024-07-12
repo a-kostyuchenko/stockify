5fb73203ab6efda4dedf7483ec22c5fba426b65a
Alexey Kostyuchenko <kosttchka@gmail.com>
2024-07-12 23:32:40 +0200

    Handling user registration in Risks module

---
+++ src/Modules/Risks/Stockify.Modules.Risks.Presentation/Individuals/UserRegisteredIntegrationEventHandler.cs
@@ -0,0 +1,30 @@
+using MediatR;
+using Stockify.Common.Application.EventBus;
+using Stockify.Common.Application.Exceptions;
+using Stockify.Common.Domain;
+using Stockify.Modules.Risks.Application.Individuals.Commands.Create;
+using Stockify.Modules.Risks.Domain.Individuals;
+using Stockify.Modules.Users.IntegrationEvents;
+
+namespace Stockify.Modules.Risks.Presentation.Individuals;
+
+internal sealed class UserRegisteredIntegrationEventHandler(ISender sender)
+    : IntegrationEventHandler<UserRegisteredIntegrationEvent>
+{
+    public override async Task Handle(
+        UserRegisteredIntegrationEvent integrationEvent,
+        CancellationToken cancellationToken = default)
+    {
+        Result result = await sender.Send(new CreateIndividualCommand(
+            IndividualId.FromValue(integrationEvent.UserId),
+            integrationEvent.FirstName,
+            integrationEvent.LastName,
+            integrationEvent.Email),
+            cancellationToken);
+
+        if (result.IsFailure)
+        {
+            throw new StockifyException(nameof(CreateIndividualCommand), result.Error);
+        }
+    }
+}
