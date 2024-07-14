7fd1a4da2c2f24c423e4a56d445b976eaaef4a62
Alexey Kostyuchenko <kosttchka@gmail.com>
2024-07-14 15:07:19 +0200

    Session entity configuration

---
+++ src/Modules/Risks/Stockify.Modules.Risks.Infrastructure/Database/Configurations/SessionConfiguration.cs
@@ -0,0 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Stockify.Modules.Risks.Domain.Individuals;
+using Stockify.Modules.Risks.Domain.Sessions;
+using Stockify.Modules.Risks.Infrastructure.Database.Constants;
+
+namespace Stockify.Modules.Risks.Infrastructure.Database.Configurations;
+
+internal sealed class SessionConfiguration : IEntityTypeConfiguration<Session>
+{
+    public void Configure(EntityTypeBuilder<Session> builder)
+    {
+        builder.ToTable(TableNames.Sessions);
+
+        builder.HasKey(s => s.Id);
+
+        builder.Property(s => s.Id)
+            .HasConversion(
+                sessionId => sessionId.Value,
+                value => SessionId.FromValue(value));
+
+        builder.Property(s => s.Status)
+            .IsRequired()
+            .HasConversion(status => status.Name, name => SessionStatus.FromName(name));
+
+        builder.HasMany(s => s.Questions)
+            .WithMany()
+            .UsingEntity(joinBuilder =>
+            {
+                joinBuilder.ToTable("session_questions");
+            });
+
+        builder.HasOne<Individual>()
+            .WithMany()
+            .HasForeignKey(s => s.IndividualId)
+            .IsRequired();
+    }
+}
