0f34828b26930ff770cc6948e7cc7e35549d4b71
Alexey Kostyuchenko <kosttchka@gmail.com>
2024-07-16 19:38:24 +0200

    Refactor SessionRepository to include related Questions and Answers in GetAsync method

--- src/Modules/Risks/Stockify.Modules.Risks.Infrastructure/Database/Repositories/SessionRepository.cs
+++ src/Modules/Risks/Stockify.Modules.Risks.Infrastructure/Database/Repositories/SessionRepository.cs
@@ -7,8 +7,9 @@ internal sealed class SessionRepository(RisksDbContext dbContext) : ISessionRepo
 {
     public async Task<Session?> GetAsync(SessionId id, CancellationToken cancellationToken = default) => 
         await dbContext.Sessions
-            .Include(s => s.Questions)
+            .Include(s => s.Questions).ThenInclude(q => q.Answers)
             .Include(s => s.Submissions)
+            .AsSplitQuery()
             .FirstOrDefaultAsync(s => s.Id == id, cancellationToken);
 
     public void Insert(Session session) => 
