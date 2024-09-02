4f825fc48c6818dbbed0212408957d941a38bde9
Alexey Kostyuchenko <kosttchka@gmail.com>
2024-09-02 13:11:27 +0200

    Add Risks module architecture tests

---
+++ src/Modules/Risks/tests/Stockify.Modules.Risks.ArchitectureTests/Abstractions/BaseTest.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using Stockify.Modules.Risks.Domain.Questions;
+using Stockify.Modules.Risks.Infrastructure;
+
+namespace Stockify.Modules.Risks.ArchitectureTests.Abstractions;
+
+public abstract class BaseTest
+{
+    protected static readonly Assembly ApplicationAssembly = Risks.Application.AssemblyReference.Assembly;
+    protected static readonly Assembly DomainAssembly = typeof(Question).Assembly;
+    protected static readonly Assembly InfrastructureAssembly = AssemblyReference.Assembly;
+    protected static readonly Assembly PresentationAssembly = Risks.Presentation.AssemblyReference.Assembly;
+}
---
+++ src/Modules/Risks/tests/Stockify.Modules.Risks.ArchitectureTests/Abstractions/TestResultExtensions.cs
@@ -0,0 +1,10 @@
+﻿using FluentAssertions;
+using NetArchTest.Rules;
+
+namespace Stockify.Modules.Risks.ArchitectureTests.Abstractions;
+
+internal static class TestResultExtensions
+{
+    internal static void ShouldBeSuccessful(this TestResult testResult) => 
+        testResult.FailingTypes?.Should().BeEmpty();
+}
