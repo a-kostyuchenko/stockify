ace713c2083e7b650533aa05754fc8ecc645a1ca
Alexey Kostyuchenko <kosttchka@gmail.com>
2024-09-05 12:19:31 +0200

    Add stockholder properties and static factory method

--- src/Modules/Stocks/Stockify.Modules.Stocks.Domain/Stockholders/Stockholder.cs
+++ src/Modules/Stocks/Stockify.Modules.Stocks.Domain/Stockholders/Stockholder.cs
@@ -4,5 +4,22 @@ namespace Stockify.Modules.Stocks.Domain.Stockholders;
 
 public sealed class Stockholder : Entity<StockholderId>
 {
+    private Stockholder()
+    {
+    }
     
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+    public string Email { get; private set; }
+    
+    public static Stockholder Create(StockholderId id, string firstName, string lastName, string email)
+    {
+        return new Stockholder
+        {
+            Id = id,
+            FirstName = firstName,
+            LastName = lastName,
+            Email = email,
+        };
+    }
 }
