054ebebc4b983c4509a8a49e0a0931e7585a1cea
Alexey Kostyuchenko <kosttchka@gmail.com>
2024-09-27 12:35:09 +0200

    Change endpoint route

--- src/Modules/Stocks/Stockify.Modules.Stocks.Presentation/Routes.cs
+++ src/Modules/Stocks/Stockify.Modules.Stocks.Presentation/Routes.cs
@@ -6,7 +6,7 @@ internal static class Routes
     {
         private const string BaseUri = "stocks";
         
-        internal const string GetGlobalQuote = $"{BaseUri}/quote/{{symbol:alpha}}";
+        internal const string GetGlobalQuote = $"{BaseUri}/{{symbol:alpha}}/quote";
         internal const string GetMarketStatus = $"{BaseUri}/market-status";
         internal const string GetStocksData = $"{BaseUri}/{{symbol:alpha}}";
     }
