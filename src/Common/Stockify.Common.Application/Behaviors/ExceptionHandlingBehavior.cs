9ea655bddba44d2522d0b55d2bc92c8d759684fc
Alexey Kostyuchenko <kosttchka@gmail.com>
2024-10-07 07:24:37 +0200

    Change ExceptionHandlingBehavior.cs

--- src/Common/Stockify.Common.Application/Behaviors/ExceptionHandlingBehavior.cs
+++ src/Common/Stockify.Common.Application/Behaviors/ExceptionHandlingBehavior.cs
@@ -1,9 +1,10 @@
+using System.Diagnostics.CodeAnalysis;
 using MediatR;
 using Microsoft.Extensions.Logging;
-using Stockify.Common.Application.Exceptions;
 
 namespace Stockify.Common.Application.Behaviors;
 
+[SuppressMessage("Major Code Smell", "S2139:Exceptions should be either logged or rethrown but not both")]
 internal sealed class ExceptionHandlingBehavior<TRequest, TResponse>(
     ILogger<ExceptionHandlingBehavior<TRequest, TResponse>> logger)
     : IPipelineBehavior<TRequest, TResponse> 
@@ -18,11 +19,11 @@ internal sealed class ExceptionHandlingBehavior<TRequest, TResponse>(
         {
             return await next();
         }
-        catch (Exception exception)
+        catch (Exception ex)
         {
-            logger.LogError(exception, "Unhandled exception for {RequestName}", typeof(TRequest).Name);
+            logger.LogError(ex, "Unhandled exception for {RequestName}", typeof(TRequest).Name);
 
-            throw new StockifyException(typeof(TRequest).Name, innerException: exception);
+            throw;
         }
     }
 }
